4ea6ac97c3c30d0dfbe48c30fdc5618cc6aaecda
donPuerto <don.puerto@hotmail.com>
2023-10-07 23:29:14 +0800

    ..

---
+++ pages/blog/[...slug].vue
@@ -0,0 +1,12 @@
+<template>
+  <main>
+    <ContentDoc>
+      <template #default="{ doc }">
+        <!-- <div>
+          {{ doc }}
+        </div> -->
+        <div>Hello</div>
+      </template>
+    </ContentDoc>
+  </main>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div>Index</div>
+  <NuxtLink to="/blog/">Read the blog!</NuxtLink>
+</template>
+
+<style></style>
