485059b95c07466bc6e81af6b43be5276c956090
donPuerto <don.puerto@hotmail.com>
2023-10-08 22:31:29 +0800

    ..

---
+++ components/Nabvar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div
+    class="p-toolbar"
+    :pt="{
+      toolbar: {
+        root: {
+          class: ['bg-gray-200']
+        }
+      }
+    }"
+  >
+    <div class="p-toolbar-group-start">Div</div>
+  </div>
+</template>
+
+<style></style>
