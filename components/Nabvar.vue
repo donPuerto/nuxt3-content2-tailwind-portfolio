8c5d2217e4ad3731ad51b72d6226fc0e0350eb30
donPuerto <don.puerto@hotmail.com>
2023-10-09 23:20:49 +0800

    ..

--- components/Nabvar.vue
+++ components/Nabvar.vue
@@ -1,16 +1,56 @@
 <template>
-  <div
-    class="p-toolbar"
+  <Toolbar
     :pt="{
-      toolbar: {
-        root: {
-          class: ['bg-gray-200']
-        }
+      root: {
+        style: { background: 'transparent', border: 'none' }
       }
     }"
   >
-    <div class="p-toolbar-group-start">Div</div>
-  </div>
+    <template #start>
+      <ul class="flex flex-grow space-x-4">
+        <li>
+          <NuxtLink
+            to="/"
+            class="flex items-center space-x-3 text-gray-700 md:mr-5"
+          >
+            <img
+              class="w-8"
+              src="~/assets/img/logo.png"
+              alt="https://www.svgshapes.in/"
+              :height="350"
+              :width="250"
+            />
+            <span class="text-gray-200 hover:text-gray-300 text-1xl font-semibold py-1">donpuerto.ph</span>
+          </NuxtLink>
+        </li>
+      </ul>
+    </template>
+    <template #center>
+      <ul class="flex items-center space-x-5">
+        <li class="hidden sm:flex">
+          <NuxtLink
+            to="/"
+            class="text-gray-200 hover:text-gray-400 text font-semibold py-1 px-3"
+            >Home</NuxtLink
+          >
+        </li>
+        <li class="hidden sm:flex">
+          <NuxtLink
+            to="/portfolio/"
+            class="text-gray-200 hover:text-gray-400 text font-semibold py-1 px-3"
+            >Portfolio</NuxtLink
+          >
+        </li>
+        <li class="hidden sm:flex">
+          <NuxtLink
+            to="/blog/"
+            class="text-gray-200 hover:text-gray-400 text font-semibold py-1 px-3"
+            >Blog</NuxtLink
+          >
+        </li>
+      </ul>
+    </template>
+  </Toolbar>
 </template>
 
 <style></style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,10 @@
-import { Nabvar } from '../.nuxt/components';
 <template>
   <div>
     <img
-      class="h-full w-auto top-0 -mt-64 md:-mt-48 lg:-mt-16 xl:mt-0"
+      class="w-auto"
       src="~/assets/img/bg.svg"
-      width="1920"
-      height="1080"
       alt="Blue and purple background graphics"
+      height="20px"
     />
     <div class="absolute h-full w-full top-0">
       <Nabvar />
