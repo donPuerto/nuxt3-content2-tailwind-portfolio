ebab7c05d23ba5ecd39a2d3a441a8f06027bcb49
donPuerto <don.puerto@hotmail.com>
2023-10-12 15:04:11 +0800

    ..

---
+++ components/ThemeSwitcher.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+  const iconClass = ref('pi-moon')
+
+  const onThemeToggler = () => {
+    const root = document.getElementsByTagName('html')[0]
+
+    root.classList.toggle('dark')
+    iconClass.value = iconClass.value === 'pi-moon' ? 'pi-sun' : 'pi-moon'
+  }
+</script>
+
+<template>
+  <button
+    type="button"
+    class="flex border-1 w-2rem h-2rem p-0 align-center justify-center"
+    @click="onThemeToggler"
+  >
+    <i :class="`dark:text-white pi ${iconClass}`" />
+  </button>
+</template>
