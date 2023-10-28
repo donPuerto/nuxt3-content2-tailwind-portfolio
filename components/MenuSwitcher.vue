224b5cbcd8244543ffa01d1b666687ebf035e69f
donPuerto <don.puerto@hotmail.com>
2023-10-28 23:34:21 +0800

    ..

---
+++ components/MenuSwitcher.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+  const emit = defineEmits(['emitSwitch'])
+  const iconClass = ref('pi-bars')
+
+  const handleMenuSwitch = () => {
+    iconClass.value = iconClass.value === 'pi-bars' ? 'pi-times' : 'pi-bars'
+    emit('emitSwitch', iconClass.value === 'pi-times')
+  }
+</script>
+
+<template>
+  <button
+    text
+    rounded
+    type="button"
+    class="border-none bg-transparent bg-transparent transition-all duration-300 ease-in-out transform hover:scale-110"
+    @click="handleMenuSwitch"
+  >
+    <i :class="`dark:text-white pi ${iconClass}`" />
+  </button>
+</template>
---
+++ components/icons/BurgerMenu.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="32"
+      height="32"
+      viewBox="0 0 512 512"
+    >
+      <path
+        fill="currentColor"
+        d="M80 96h352v32H80zm0 144h352v32H80zm0 144h352v32H80z"
+      />
+    </svg>
+  </div>
+</template>
+
+<style></style>
