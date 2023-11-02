fa6ea0e709ecfacf440eba5792c266d2c497c154
donPuerto <don.puerto@hotmail.com>
2023-11-02 21:36:23 +0800

    ..

--- components/Hero.vue
+++ components/Hero.vue
@@ -2,12 +2,12 @@
   <section class="text-white absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full">
     <div class="max-w-full p-4 text-center">
       <h1
-        class="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text text-6xl font-extrabold text-transparent sm:text-5xl"
+        class="text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text text-transparent font-extrabold"
       >
         Don Puerto &trade;
       </h1>
       <p class="text-gray-300 ml-[-20px]">Computer Engineer</p>
-      <p class="mx-auto mt-6 max-w-4xl sm:text-xl/relaxed">
+      <p class="mx-auto mt-4 max-w-4xl sm:text-xl/relaxed">
         A Fullstack Engineer with expertise in Vue/Nuxt, Supabase, and Flutter excels in creating web and mobile
         applications that offer a unified user experience across platforms.
       </p>
