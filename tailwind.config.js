e58096a4fe776ddc60c8a3149dc021ba5a0dd7a1
donPuerto <don.puerto@hotmail.com>
2023-11-15 09:12:19 +0800

    ..

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,8 @@ export default {
     './pages/**/*.vue',
     './plugins/**/*.{js,ts}',
     './nuxt.config.{js,ts}',
+    './index.html',
+    './src/**/*.{vue,js,ts,jsx,tsx}',
     './node_modules/primevue/**/*.{vue,js,ts,jsx,tsx}'
   ],
   theme: {
