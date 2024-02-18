b03aa04d72a69b320bf6e98aafff5617236de4d5
donPuerto <don.puerto@hotmail.com>
2024-02-19 07:16:47 +0800

    ..

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,30 +1,57 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  pages: true,
+  ssr: true,
+
+  app: {
+    pageTransition: { name: 'page', mode: 'out-in' }
+  },
+  site: {
+    url: process.env.NUXT_PUBLIC_SITE_URL,
+    name: 'Don Puerto Portfolio',
+    description: 'Don Puerto | My Website Portfolio | Fullstack Developer',
+    defaultLocale: 'en', // not needed if you have @nuxtjs/i18n installed
+    indexable: true
+  },
+  sitemap: {
+    strictNuxtContentPaths: true
+  },
+
+  image: {
+    quality: 90,
+    format: ['webp']
+  },
+
+  // ui: {
+  //   global: true,
+  //   icons: ['material-symbols', 'mdi', 'ri', 'ph']
+  // },
+
   css: [],
   typescript: {
     strict: true
   },
-  modules: ['@nuxtjs/tailwindcss', '@nuxt/content', '@nuxt/devtools'],
-  content: {
-    // ... options
-    highlight: {
-      theme: 'github-dark',
-      // Define languages you expect to use
-      preload: ['java', 'javascript']
-    },
-    markdown: {
-      // Configuring external link processing
-      // https://github.com/rehypejs/rehype-external-links
-      rehypePlugins: [
-        [
-          'rehype-external-links',
-          {
-            target: '_blank',
-            rel: 'noopener noreferer'
-          }
-        ]
-      ]
-    }
-  }
+  modules: ['@nuxtjs/tailwindcss', '@nuxt/devtools', '@nuxtjs/seo', '@nuxt/image']
+  // content: {
+  //   // ... options
+  //   highlight: {
+  //     theme: 'github-dark',
+  //     // Define languages you expect to use
+  //     preload: ['java', 'javascript']
+  //   },
+  //   markdown: {
+  //     // Configuring external link processing
+  //     // https://github.com/rehypejs/rehype-external-links
+  //     rehypePlugins: [
+  //       [
+  //         'rehype-external-links',
+  //         {
+  //           target: '_blank',
+  //           rel: 'noopener noreferer'
+  //         }
+  //       ]
+  //     ]
+  //   }
+  // }
 })
