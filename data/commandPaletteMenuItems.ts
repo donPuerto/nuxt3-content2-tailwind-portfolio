eb2e0c1d6c0e76d7b5dc74c88d8a3c16acb649fe
donPuerto <don.puerto@hotmail.com>
2024-06-18 20:15:17 +0800

    ..

--- data/commandPaletteMenuItems.ts
+++ data/commandPaletteMenuItems.ts
@@ -11,16 +11,16 @@ export const menuItems: Ref<MenuItem[]> = ref([
   {
     category: 'NAVIGATION',
     items: [
-      { icon: 'i-line-md-home', name: 'Home', shortcut: ['⌘', 'H'] },
-      { icon: 'i-line-md-document-twotone', name: 'Blog', shortcut: ['⌘', 'B'] },
-      { icon: 'i-line-md-person', name: 'About', shortcut: ['⌘', 'A'] },
+      { icon: 'line-md:home-md', name: 'Home', shortcut: ['⌘', 'H'] },
+      { icon: 'line-md:document-list-twotone', name: 'Blog', shortcut: ['⌘', 'B'] },
+      { icon: 'line-md:person', name: 'About', shortcut: ['⌘', 'A'] },
     ],
   },
   {
     category: 'SOCIAL CONNECTION',
     items: [
-      { icon: 'i-line-md-github', name: 'Github', shortcut: ['⌘', 'G'] },
-      { icon: 'i-line-md-twitter-x', name: 'Twitter X', shortcut: ['⌘', 'X'] },
+      { icon: 'line-md:github-loop', name: 'Github', shortcut: ['⌘', 'G'] },
+      { icon: 'line-md:twitter-x-alt', name: 'Twitter X', shortcut: ['⌘', 'X'] },
     ],
   },
 ])
