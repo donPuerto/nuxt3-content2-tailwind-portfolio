9a1515f21e6fcb83104c8e05cf94070c07aa8631
donPuerto <don.puerto@hotmail.com>
2024-06-19 14:05:26 +0800

    ..

--- components/ParticlesBackground.vue
+++ components/ParticlesBackground.vue
@@ -1,25 +1,30 @@
+<!-- eslint-disable no-console -->
 <script setup lang="ts">
 import type { Container } from '@tsparticles/engine'
 import { MoveDirection, OutMode } from '@tsparticles/engine'
 
 // Utility function to get CSS variable value
 const getCssVariable = (variable: string) => {
-  return getComputedStyle(document.documentElement).getPropertyValue(variable).trim()
+  const value = getComputedStyle(document.documentElement).getPropertyValue(variable).trim()
+  console.log(`Fetched CSS variable ${variable}: ${value}`) // Debugging log
+  return value
 }
 
 const particlesLoaded = (container?: Container) => {
   // eslint-disable-next-line no-console
   console.log(container)
 }
-const options = ref({
+const particleOptions = ref({
   background: {
     color: {
-      value: 'var(--bg-primary-color)',
+      // Set a default value or make it transparent initially
+      value: 'tranparent',
     },
   },
   particles: {
     color: {
-      value: '#333333',
+      // Fallback value
+      value: '#ffff',
     },
     number: {
       value: 100,
@@ -49,11 +54,36 @@ const options = ref({
 
 })
 
+const themeColors = {
+  textPrimaryColor: ref(getCssVariable('--text-primary-color')),
+  bgPrimaryColor: ref(getCssVariable('--bg-primary-color')),
+}
+
+const updateParticleColors = () => {
+  particleOptions.value.particles.color.value = themeColors.textPrimaryColor.value
+  particleOptions.value.background.color.value = themeColors.bgPrimaryColor.value
+
+  console.log('Particle options updated with theme colors:', {
+    particlesColor: particleOptions.value.particles.color.value,
+    backgroundColor: particleOptions.value.background.color.value,
+  })
+}
+
+const watchThemeChanges = () => {
+  watchEffect(() => {
+    // Ensure getComputedStyle is available and run only on client side
+    if (import.meta.client) {
+      themeColors.textPrimaryColor.value = getComputedStyle(document.documentElement).getPropertyValue('--text-primary-color').trim()
+      themeColors.bgPrimaryColor.value = getComputedStyle(document.documentElement).getPropertyValue('--bg-primary-color').trim()
+
+      updateParticleColors()
+    }
+  })
+}
+
+// Watch for theme changes
 onMounted(() => {
-  // Update the particle color with the CSS variable once the component is mounted
-  options.value.particles.color.value = getCssVariable('--text-primary-color')
-  // eslint-disable-next-line no-console
-  console.log('Particles options updated with theme color:', options.value.particles.color.value)
+  watchThemeChanges()
 })
 </script>
 
@@ -61,7 +91,7 @@ onMounted(() => {
   <client-only>
     <vue-particles
       id="tsparticles"
-      :options="options"
+      :options="particleOptions"
       @particles-loaded="particlesLoaded"
     />
   </client-only>
