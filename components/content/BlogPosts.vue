e51176f264d0c401d324fda8e1ba2d5e66a18976
donPuerto <don.puerto@hotmail.com>
2024-06-20 09:55:07 +0800

    ..

---
+++ components/content/BlogPosts.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const blogPosts = await queryContent('/blog')
+  .sort({ date: -1 }) // show latest articles first
+  .where({ _partial: false }) // exclude the Partial files
+  .find()
+</script>
+
+<template>
+  <h1>Blog</h1>
+  <ul>
+    <li
+      v-for="{ _path: slug, title } in blogPosts"
+      :key="slug"
+    >
+      <NuxtLink :to="slug">
+        {{ title }}
+      </NuxtLink>
+    </li>
+  </ul>
+</template>
