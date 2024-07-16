4c4a7667abeb6e61fd6ce844e12de8e22ef5c084
Don <don.puerto@hotmail.com>
2024-07-16 16:13:33 +0800

    ..

--- types/components/blog/post.d.ts
+++ types/components/blog/post.d.ts
@@ -1,3 +1,9 @@
+export interface Author {
+  name: string
+  avatar: string
+  slug: string
+}
+
 export interface Post {
   _path: string
   slug: string
@@ -6,13 +12,11 @@ export interface Post {
   subtitle?: string
   series?: string
   description?: string
-  body: string | MarkdownRoot
   image: string
   alt?: string
   published_on: string
   is_publish: boolean
-  author: string
-  author_avatar: string
+  authors: Author | Author[] | string
   reading_time: string
   tag: string[]
 }
