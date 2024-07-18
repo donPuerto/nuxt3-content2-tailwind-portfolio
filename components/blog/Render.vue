70c7db9ef3bc22958333e196c69b1b03d6da12a6
Don <don.puerto@hotmail.com>
2024-07-18 15:04:22 +0800

    ..

--- components/blog/Render.vue
+++ components/blog/Render.vue
@@ -26,7 +26,7 @@ const quickLinks = ref<QuickLink[]>([
   { icon: 'ph:pen-duotone', text: 'Edit this article', url: 'https://github.com/donPuerto/nuxt3-content2-tailwind-portfolio/blob/master/content/blog/2.nuxt/1.nuxt.md' },
   { icon: 'ph:shooting-star-duotone', text: 'Star on Github', url: 'https://github.com/donPuerto' },
   { icon: 'ph:chat-centered-text-duotone', text: 'Chat on Discord', url: 'https://discord.gg/6eGKS3En' },
-  { icon: 'ph:hand-heart-duotone', text: 'Become Sponsor', url: '/about' },
+  { icon: 'ph:hand-heart-duotone', text: 'Become Sponsor', url: 'https://github.com/sponsors' },
 ])
 
 const title = computed(() => props.post.title)
