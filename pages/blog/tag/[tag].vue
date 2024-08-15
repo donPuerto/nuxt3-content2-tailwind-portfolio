278ecc5de0bf01a8876b09e3ab23c74a38964611
Don <don.puerto@hotmail.com>
2024-08-15 09:50:25 +0800

    ..

--- pages/blog/tag/[tag].vue
+++ pages/blog/tag/[tag].vue
@@ -5,6 +5,7 @@ import type { Post } from '~/types/components/blog/post'
 const route = useRoute()
 const currentPage = ref(1)
 const postsPerPage = 10
+const showAllAuthors = ref(false)
 
 // Fetch all blog posts with the given tag
 const { data: posts, pending } = await useAsyncData<Post[]>(`fetch-blog-posts-by-tag-${route.params.tag}`, () =>
@@ -133,76 +134,119 @@ const filteredPosts = computed(() =>
                   </p>
                 </div>
 
-                <!-- Author Avatar -->
+                <!-- Start: Author -->
                 <div class="md:flex md:justify-between md:items-center mb-4">
                   <div
                     v-if="allAuthors.length > 0"
-                    class="flex -space-x-4 overflow-hidden"
+                    class="relative flex overflow-hidden"
+                    @mouseenter="showAllAuthors = true"
+                    @mouseleave="showAllAuthors = false"
                   >
-                    <template
-                      v-for="(author, index) in allAuthors.slice(0, 6)"
-                      :key="index"
+                    <TransitionGroup
+                      name="author-list"
+                      tag="div"
+                      class="flex -space-x-4"
                     >
                       <div
+                        v-for="(author, index) in showAllAuthors ? allAuthors : allAuthors.slice(0, 3)"
+                        :key="author.slug || index"
                         class="flex justify-end"
                       >
-                        <UiAvatar
-                          v-if="author.avatar"
-                          :src="author.avatar"
-                          :alt="author.name"
-                          class="border-2 border-white dark:border-gray-800 hover:border-primary dark:hover:border-primary rounded-full h-10 w-10 -mr-2 last:mr-0 transition-colors duration-200 ease-in-out"
-                        />
-
                         <div
-                          class="border-2 border-white dark:border-gray-800 rounded-full h-10 w-10 -mr-2 last:mr-0 bg-primary flex items-center justify-center text-white text-sm font-semibold"
+                          class="flex justify-end"
                         >
-                          {{ author.name.charAt(0) }}
+                          <UiAvatar
+                            v-if="author.avatar"
+                            :src="author.avatar"
+                            :alt="author.name"
+                            class="border-2 border-white dark:border-gray-800 hover:border-primary dark:hover:border-primary rounded-full h-10 w-10 -mr-2 last:mr-0 transition-colors duration-200 ease-in-out"
+                          />
+
+                          <div
+                            v-else
+                            class="border-2 border-white dark:border-gray-800 rounded-full h-10 w-10 -mr-2 last:mr-0 bg-primary flex items-center justify-center text-white text-sm font-semibold"
+                          >
+                            {{ author.name.charAt(0) }}
+                          </div>
                         </div>
-                        <span
-                          v-if="allAuthors.length > 6"
-                          class="flex items-center justify-center bg-white dark:bg-gray-800 text-sm text-gray-800 dark:text-white font-semibold border-2 border-gray-200 dark:border-gray-700 rounded-full h-10 w-10"
-                        >
-                          +{{ allAuthors.length - 6 }}
-                        </span>
                       </div>
-                    </template>
+                    </TransitionGroup>
 
-                    <div
-                      v-if="filteredPosts.length === 0 && !pending"
-                      class="bg-white rounded-lg shadow-md overflow-hidden flex items-center justify-center p-8"
-                    >
-                      <p class="text-gray-600 text-center">
-                        No posts found with the tag "{{ route.params.tag }}".
-                      </p>
-                    </div>
-                  </div>
-
-                  <!-- Pagination -->
-                  <div class="mt-4">
-                    <button
-                      v-for="page in totalPages"
-                      :key="page"
-                      :class="[
-                        'px-4 py-2 mx-1 rounded-md focus:outline-none',
-                        currentPage === page ? 'bg-blue-500 text-white' : 'bg-gray-200 text-gray-700 hover:bg-gray-300',
-                      ]"
-                      @click="currentPage = page"
-                    >
-                      {{ page }}
-                    </button>
+                    <Transition name="fade">
+                      <span
+                        v-if="allAuthors.length > 3 && !showAllAuthors"
+                        class="absolute right-0 flex items-center justify-center bg-white dark:bg-gray-800 text-sm text-gray-800 dark:text-white font-semibold border-2 border-gray-200 dark:border-gray-700 hover:border-primary dark:hover:border-primary rounded-full h-10 w-10 transition-colors duration-200 ease-in-out z-20 cursor-pointer"
+                      >
+                        +{{ allAuthors.length - 3 }}
+                      </span>
+                    </Transition>
                   </div>
                 </div>
+                <!-- End: Author Avatar -->
+
+                <!-- Start: Show card that has no tag -->
+                <div
+                  v-if="filteredPosts.length === 0 && !pending"
+                  class="bg-white rounded-lg shadow-md overflow-hidden flex items-center justify-center p-8"
+                >
+                  <p class="text-gray-600 text-center">
+                    No posts found with the tag "{{ route.params.tag }}".
+                  </p>
+                </div>
+                <!-- End: Show card that has no tag -->
               </div>
             </div>
           </div>
         </div>
       </div>
+
+      <!-- Pagination -->
+      <div class="mt-4">
+        <button
+          v-for="page in totalPages"
+          :key="page"
+          :class="[
+            'px-4 py-2 mx-1 rounded-md focus:outline-none',
+            currentPage === page ? 'bg-blue-500 text-white' : 'bg-gray-200 text-gray-700 hover:bg-gray-300',
+          ]"
+          @click="currentPage = page"
+        >
+          {{ page }}
+        </button>
+      </div>
     </div>
   </div>
 </template>
 
-<style scoped>
-.avatar-wrapper {
-  transition: transform 0.2s ease-in-out, z-index 0.2s ease-in-out;
+Here's the modified CSS to change the transition from left to right:
+vueCopy<style scoped>
+.author-list-move,
+.author-list-enter-active,
+.author-list-leave-active {
+  transition: all 0.3s ease;
+}
+
+.author-list-enter-from {
+  opacity: 0;
+  transform: translateX(-20px);
+}
+
+.author-list-leave-to {
+  opacity: 0;
+  transform: translateX(20px);
+}
+
+.author-list-leave-active {
+  position: absolute;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
 }
 </style>
