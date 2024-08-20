544d86cea7c9d917a9f3748973fc49639cc4eb60
Don <don.puerto@hotmail.com>
2024-08-20 22:07:14 +0800

    ..

--- components/tag/v1/Grid.vue
+++ components/tag/v1/Grid.vue
@@ -21,27 +21,29 @@ const {
 </script>
 
 <template>
-  <div class="flex flex-col min-h-screen">
-    <div class="flex-grow">
-      <div class="container mx-auto px-4 h-full">
-        <div class="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-4 gap-4 h-full">
-          <!-- Start: Tag Card -->
-          <template v-if="posts.length > 0">
-            <TagV1Card
-              v-for="post in posts"
-              :key="post.slug"
-              :post="post"
-            />
-          </template>
-          <!-- End: Tag Card -->
+  <!-- Root -->
+  <div>
+    <div class="flex flex-col min-h-screen">
+      <div class="flex-grow">
+        <div class="container mx-auto px-4 h-full">
+          <div class="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-4 gap-4 h-full">
+            <!-- Start: Tag Card -->
+            <template v-if="posts.length > 0">
+              <TagV1Card
+                v-for="post in posts"
+                :key="post.slug"
+                :post="post"
+              />
+            </template>
+            <!-- End: Tag Card -->
 
-          <!-- Start: Tag not found card h-full -->
-          <template v-else>
-            <div class="md:col-span-1 lg:col-span-4 flex items-center justify-center">
-              <!-- Your existing card content -->
-              <div
-                ref="targetRef"
-                class="
+            <!-- Start: Tag not found card h-full -->
+            <template v-else>
+              <div class="md:col-span-1 lg:col-span-4 flex items-center justify-center">
+                <!-- Your existing card content -->
+                <div
+                  ref="targetRef"
+                  class="
                   relative
                   flex
                   h-60
@@ -53,36 +55,37 @@ const {
                   border border-border
                   bg-gradient-to-r from-black to-gray-950
                   shadow-2xl"
-                @mousemove="handleMouseMove"
-                @focus="handleFocus"
-                @blur="handleBlur"
-                @mouseenter="handleMouseEnter"
-                @mouseleave="handleMouseLeave"
-              >
-                <div
-                  class="pointer-events-none absolute inset-0 transition duration-300"
-                  :style="{
-                    opacity,
-                    background: `radial-gradient(600px circle at ${position.x}px ${position.y}px, rgba(255,182,255,.1), transparent 40%)`,
-                  }"
-                />
-                <div class="relative z-10 flex flex-col items-center text-center">
-                  <Icon
-                    name="material-symbols:feature-search-outline"
-                    size="52px"
-                    class="mb-4"
+                  @mousemove="handleMouseMove"
+                  @focus="handleFocus"
+                  @blur="handleBlur"
+                  @mouseenter="handleMouseEnter"
+                  @mouseleave="handleMouseLeave"
+                >
+                  <div
+                    class="pointer-events-none absolute inset-0 transition duration-300"
+                    :style="{
+                      opacity,
+                      background: `radial-gradient(600px circle at ${position.x}px ${position.y}px, rgba(255,182,255,.1), transparent 40%)`,
+                    }"
                   />
-                  <h3 class="text-base font-medium lg:text-base mb-2">
-                    No tags found
-                  </h3>
-                  <p class="mb-3 leading-relaxed text-sm lg:text-sm text-center">
-                    No posts with the searched tag were found.
-                  </p>
+                  <div class="relative z-10 flex flex-col items-center text-center">
+                    <Icon
+                      name="material-symbols:feature-search-outline"
+                      size="52px"
+                      class="mb-4"
+                    />
+                    <h3 class="text-base font-medium lg:text-base mb-2">
+                      No tags found
+                    </h3>
+                    <p class="mb-3 leading-relaxed text-sm lg:text-sm text-center">
+                      No posts with the searched tag were found.
+                    </p>
+                  </div>
                 </div>
               </div>
-            </div>
-          </template>
+            </template>
           <!-- End: Tag not found card h-full -->
+          </div>
         </div>
       </div>
     </div>
