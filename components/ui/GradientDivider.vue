b774df6443b17c9c498035c8e47ad60e1cefd952
Don <don.puerto@hotmail.com>
2024-09-01 07:04:44 +0800

    ..

--- components/ui/GradientDivider.vue
+++ components/ui/GradientDivider.vue
@@ -1,26 +1,29 @@
-<template>
-  <Separator v-bind="forwarded" :class="styles({ class: props.class })" />
-</template>
-
 <script lang="ts" setup>
-  import { Separator } from "radix-vue";
-  import type { SeparatorProps } from "radix-vue";
+import { Separator } from 'radix-vue'
+import type { SeparatorProps } from 'radix-vue'
 
-  const props = withDefaults(
-    defineProps<
-      SeparatorProps & {
-        /** Custom class(es) to add to parent element */
-        class?: any;
-      }
-    >(),
-    {
-      as: "hr",
+const props = withDefaults(
+  defineProps<
+    SeparatorProps & {
+      /** Custom class(es) to add to parent element */
+      class?: any
     }
-  );
+  >(),
+  {
+    as: 'hr',
+  },
+)
 
-  const forwarded = reactiveOmit(props, "class");
+const forwarded = reactiveOmit(props, 'class')
 
-  const styles = tv({
-    base: 'h-px w-full border-t-0 bg-[linear-gradient(90deg,theme("colors.input/10%"),_theme("colors.input"),_theme("colors.input/10%"))]',
-  });
+const styles = tv({
+  base: 'h-px w-full border-t-0 bg-[linear-gradient(90deg,theme("colors.input/10%"),_theme("colors.input"),_theme("colors.input/10%"))]',
+})
 </script>
+
+<template>
+  <Separator
+    v-bind="forwarded"
+    :class="styles({ class: props.class })"
+  />
+</template>
